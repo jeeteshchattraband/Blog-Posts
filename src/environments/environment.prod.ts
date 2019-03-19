84c0d6171e7ed505f0ec3a16006ed519bcbd6d34
challa john dhinakar reddy <39598552+dhinakar777@users.noreply.github.com>
2019-03-19 22:01:01 +0530

    My First Blog

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCEto5oii0W1Cf_ahopnN_2NKh3hNWFMJ0",
+    authDomain: "blog-posts-d5e42.firebaseapp.com",
+    databaseURL: "https://blog-posts-d5e42.firebaseio.com",
+    projectId: "blog-posts-d5e42",
+    storageBucket: "blog-posts-d5e42.appspot.com",
+    messagingSenderId: "477409163938"
+  }
+};
